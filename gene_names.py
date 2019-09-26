c81d216bcd0528ffc3c88d31aa8ce08ea1026dfb
maiacorpuz <54734062+maiacorpuz@users.noreply.github.com>
2019-09-26 08:21:05 -0700

    Update gene_names.py

--- gene_names.py
+++ gene_names.py
@@ -3,17 +3,25 @@ import sys
 import fileinput
 import re
 
-my_file= sys.argv[1]
-
-for each_line_of_text in fileinput.input(my_file):
-  if re.match(r'.*\t.*\tgene\t', each_line_of_text):
-        gene_name_matches = re.findall ('gene_name \"(.*?)\";', each_line_of_text)
-        chromosome_matches = re.findall ('^.', each_line_of_text)
-        startPos_matches = re.findall ('.*\t.*\tgene\t(\d\d\d\d\d)\s', each_line_of_text)
-        endPos_matches = re.findall ('.*\t.*\tgene\t(\d\d\d\d\d)\s(\d\d\d\d\d)', each_line_of_text)
-[
-        print 'gene_name', + gene_name_matches
-        print 'chromosome' + chromosome_matches
-        print 'startPos ' + startPos_matches
-        print 'endPos ' + endPos_matches
-]
+output = {}
+f = open (sys.argv[1],'r')
+lines=f.readlines()
+for each_line_of_text in lines:
+  if re.search(r'.*\t.*\tgene\t', each_line_of_text):
+        geneName = re.findall (r'gene_name \"(.*?)\";', each_line_of_text)
+        print "{\"geneName\":",
+        print geneName,
+        print ",",
+        chromosome = re.findall (r'^(\d)', each_line_of_text)
+        print "{\chromosome\":",
+        print chromosome,
+        print ",",
+        startPos = re.findall (r'.*\t.*\tgene\t(\d\d\d\d\d)\s', each_line_of_text)
+        print "{\startPos\":",
+        print startPos,
+        print ",",
+        endPos = re.findall (r'.*\t.*\tgene\t(\d\d\d\d\d)\s(\d\d\d\d\d)', each_line_of_text)
+        print "{\endPos\":",
+        print endPos,
+        print ",",
+        print "}"
