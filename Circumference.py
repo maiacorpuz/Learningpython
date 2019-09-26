18706df6d12a37357a039750a13055a69e7435b1
maiacorpuz <54734062+maiacorpuz@users.noreply.github.com>
2019-09-25 22:31:46 -0700

    Create Circumference.py

---
+++ Circumference.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+bool = True
+pi = 3.14159
+print(str(pi) + ' is the value of pi.' )
+a = int(raw_input("Enter a number: "))
+print( "The circumference of a radius " + str(a) + ' is ' )
+a = b = pi
+a = ((2 * pi) * b)
+print a
